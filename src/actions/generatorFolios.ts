738c1127e636c82e081d992ce806408f4e8b21dd
Josue <ajpm@thinkcares.com>
2025-06-17 13:27:23 -0600

    Function new

--- src/actions/generatorFolios.ts
+++ src/actions/generatorFolios.ts
@@ -1,15 +1,18 @@
-import { CreateFolios } from "../estafetaAPI/folios"
-import type { Folios } from "../interface/services.interface"
+import { CreateFolios } from "../estafetaAPI/folios";
+import type { Folios } from "../interface/services.interface";
 
 export const generatorFolios = async (orderNumber: string) => {
-  try{
-    const folios = await CreateFolios(parseInt(orderNumber))
-    if(!folios.data.success) return console.error("Folios no pudieron ser recuperados")
-    const foliosResults: Folios[] = folios.data.folioResult
-    for(const folio of foliosResults) {
-      console.log(`Q3SQR${folio.folioMD5}`)
+  try {
+    const folios = await CreateFolios(parseInt(orderNumber));
+    if (!folios.data.success)
+      return console.error("Folios no pudieron ser recuperados");
+    const foliosResults: Folios[] = folios.data.folioResult;
+    for (const folio of foliosResults) {
+      console.log(`Q3SQR${folio.folioMD5}`);
     }
-  } catch(err: any){
-    console.log(err)
+  } catch (err: any) {
+    console.log(err);
   }
-}
+};
+
+export const nada = () => {};
